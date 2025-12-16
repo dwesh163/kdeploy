9f892f11e9d6664f32e9c080af0d02a776401d18
dwehs163 <dwesh163@protonmail.com>
2025-12-16 11:01:12 +0100

    [add] initialize kdeploy module with version, author, and license information

---
+++ kdeploy/__init__.py
@@ -0,0 +1,9 @@
+"""kdeploy - Extensible Kubernetes deployment CLI tool."""
+
+__version__ = "0.1.0"
+__author__ = "dwesh163"
+__license__ = "MIT"
+
+from kdeploy.cli import main
+
+__all__ = ["main"]
